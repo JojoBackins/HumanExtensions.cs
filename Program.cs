eb510613e887f1aaf70b7476a29d3796eb1e195b
jojogreen <jojogreen1408@gmail.com>
2024-03-04 18:26:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AmazingExtensions
+{
+    public static class ExtendAHuman
+    {
+        public static bool IsDistressCall(this string s)
+        {
+            if (s.Contains("Help!"))
+                return true;
+            else
+                return false;
+        }
+    }
+}
+namespace Human
+{
+    using AmazingExtensions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Evil clones are wreaking havoc. Help!";
+            message.IsDistressCall();
+
+        }
+    }
+}
